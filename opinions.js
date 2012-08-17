b3835becc8df1eef084c7e62be7deb9ae13a2326
Paul Vorbach <paul@vorb.de>
2012-08-17 02:09:14 +0200

    Add queue

--- opinions.js
+++ opinions.js
@@ -2,57 +2,79 @@ var fs = require('fs');
 
 var async = require('async');
 var Git = require('git-wrapper');
+var logger = require('stoopid').logger('opinions');
+var Queue = require('miniqueue').Queue;
 var Set = require('Set');
 
-var Opinions = module.exports = function Options(options) {
+var Opinions = module.exports = function Opinions(options) {
   'use strict';
-  this.git = new Git();
+  var self = this;
+  self.git = new Git(options);
+  self.logger = logger;
+  self.queue = new Queue(function (args, release) {
+    'use strict';
+    // restore arguments
+    var forFile = args[0];
+    var comment = args[1];
+    var tasks   = args[2];
+    var added   = args[3];
+
+    if (typeof forFile != 'string')
+      return release() && added(new Error('forFile must be a String.'));
+    if (typeof added != 'function')
+      return release()
+        && added(new Error('added must be a callback function.'));
+
+    var path = forFile += '.comments';
+
+    var toCommit = new Set();
+
+    // execute tasks
+    async.forEach(tasks, function execute(task, callback) {
+      'use strict';
+      task(forFile, comment, function (err, changed) {
+        'use strict';
+        if (err)
+          return callback(err);
+
+        toCommit.addAll(changed);
+        callback();
+      });
+    }, function cb(err) {
+      'use strict';
+      if (err)
+        return release() && added(err);
+
+      // start committing the files
+      self.git.exec('add', toCommit.toArray(), function (err, out) {
+        'use strict';
+        if (err)
+          return release() && added(err);
+
+        var msg = 'Add comment for\n\n'+forFile;
+
+        self.git.exec('commit', function (err, out) {
+          'use strict';
+          if (err)
+            return release() && added(err);
+
+          release();
+          added();
+        });
+      });
+    });
+  });
+
+  self.queue.on('empty', function () {
+    self.logger.info('queue empty');
+  });
 };
 
 opinions.prototype.addComment =
     function addComment(forFile, comment, tasks, added) {
   'use strict';
 
-  if (typeof forFile != 'string')
-    return added(new Error('forFile must be a String.'));
-  if (typeof added != 'function')
-    return added(new Error('added must be a callback function.'));
-
-  var path = forFile += '.comments';
-
-  var toCommit = new Set();
-
-  // execute tasks
-  async.forEach(tasks, function execute(task, callback) {
-    'use strict';
-    task(forFile, comment, function (err, changed) {
-      'use strict';
-      if (err)
-        return callback(err);
-
-      toCommit.addAll(changed);
-      callback();
-    });
-  }), function cb(err) {
-    'use strict';
-    if (err)
-      return added(err);
-
-    // start committing the files
-    this.git.exec('add', toCommit.toArray(), function (err, out) {
-      'use strict';
-      if (err)
-        return added(err);
-
-      var msg = 'Add comment for\n\n'+forFile;
-
-      this.git.exec('commit', function (err, out) {
-        'use strict';
-        if (err)
-          return added(err);
-
-        added();
-      });
-    });
-  };
+  this.logger.info('init comment for file ', file);
+  // add the comment to the queue
+  this.queue.add(arguments);
 };
